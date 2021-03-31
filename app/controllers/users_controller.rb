70662d613712cef1881e559d99549eb0c7d90aea
Maciekgit config --global user.name Maciekgit config --global user.name Maciek <mopalskilowicz@gmail.com>
2021-03-31 13:27:41 +0200

    Display user/friend profile and tracking options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
+    @user = current_user
   end
   def my_friends
     @friends = current_user.friends
@@ -26,5 +27,10 @@ class UsersController < ApplicationController
         format.js { render partial: 'users/friend_result' }
       end
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 end
